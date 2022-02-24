f0ddd32ac2a5c2887ddfb2b13d04364355597168
Nathaniel Chen <nathanielchen@hey.com>
2022-02-24 14:33:42 -0800

    Add a handle to the selected node
    The handle allows us to draw edges between nodes.
    
    The 'cytoscape-popper' library lets us add arbitrary
    elements to nodes.

--- src/client/components/Editor.js
+++ src/client/components/Editor.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import edgehandles from 'cytoscape-edgehandles';
+import popper from 'cytoscape-popper';
 import testgraph from './testgraph.json';
 import styles from './cy.css';
 
@@ -23,8 +24,13 @@ class Editor extends React.Component {
     }
 
     componentDidMount = () => {
-        //TODO CY select listener
-        console.log(cytoscape);
+        // Handle is shared by all nodes
+        const div = document.createElement('div');
+        div.id = 'handle';
+        div.className = 'p-2 border-2 border-blue-700 rounded';
+        div.hidden = true;
+        document.body.appendChild(div);
+
         //cytoscape.use(edgehandles);
         const cy = window.cy = cytoscape({
             container: document.getElementById('cy'),
@@ -43,10 +49,54 @@ class Editor extends React.Component {
                 }
             ]
         });
-        console.log(cy);
         const eh = cy.edgehandles({
             snap: true
         });
+
+        let selectedNodes = cy.collection();
+        this.setState({
+            selectedNodes
+        });
+
+        // Select node
+        const selectNode = (node) => {
+            node.handle = node.popper({
+                content: () => {
+                    div.hidden = false;
+                    return div;
+                }
+            });
+
+            // Add to empty collection so that we
+            // only select one node at a time
+            this.setState({
+                selectedNodes: cy.collection().union(node)
+            });
+        }
+
+        cy.on('click', 'node', (evt) => {
+            const node = evt.target;
+            selectNode(node);
+        });
+
+        // Move node
+        cy.on('position', 'node', (evt) => {
+            const node = evt.target;
+            // Update handle position
+            if (node.handle) {
+                node.handle.update();
+            }
+        });
+
+        // Update handle when grid moves
+        cy.on('pan zoom resize', () => {
+            // Get selected node
+            const node = this.state.selectedNodes.first();
+            // Update handle position
+            if (node.handle) {
+                node.handle.update();
+            }
+        })
     }
 
     render() {
