0216257be42cf9e1813446e4b82eee2c82ab9adf
Nathaniel Chen <nathanielchen@hey.com>
2022-02-25 18:54:18 -0800

    Dark mode, delete edges, Sidebar multi-line

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# gitmaps
+# GitMaps
+
+Turn any GitHub repository into a collaborative graph.
+
+1. Add a file called 'gitmap.json' to your repository
+
+
+# Examples
+
+
--- src/client/components/Sidebar.js
+++ src/client/components/Sidebar.js
@@ -22,9 +22,22 @@ class Sidebar extends React.Component {
         this.setState({ mode: SidebarMode.Edit });
     }
 
+    renderText = (text) => {
+        if (!text) {
+            return;
+        }
+        const lines = text.split('\n').map((line, idx) => {
+            return <p key={idx}>{line}</p>
+        });
+
+        return lines;
+    }
+
     render() {
+        const text = this.renderText(this.props.node?.description);
+
         return (
-            <div className="absolute z-10 h-full w-1/3 right-0 top-0 bg-blue-300 shadow-lg px-6">
+            <div className="absolute z-10 h-full w-1/3 right-0 top-0 bg-blue-200 shadow-lg px-6">
                 {(this.state.mode === SidebarMode.View) &&
                     <>
                         {(!this.props.noEditing) &&
@@ -38,8 +51,7 @@ class Sidebar extends React.Component {
                             </div>
                         }
                         <div className="mt-4">
-                            <span className="">Title:</span>
-                            <div className="flex justify-center items-center mt-4">
+                            <div className="flex justify-center items-center mt-4 font-semibold text-xl">
                                 <div
                                     className="w-full"
                                 >
@@ -48,12 +60,11 @@ class Sidebar extends React.Component {
                             </div>
                         </div>
                         <div className="mt-4">
-                            <span className="">Description:</span>
                             <div className="flex justify-center items-center mt-4">
                                 <div
                                     className="w-full"
                                 >
-                                    {this.props.node?.description || ''}
+                                    {text}
                                 </div>
                             </div>
                         </div>
@@ -66,40 +77,32 @@ class Sidebar extends React.Component {
                                 onClick={this.viewMode}
                                 className="rounded px-2 py-1 border border-blue-700"
                             >
-                                View Mode
+                                Done
                             </button>
                         </div>
                         <div className="mt-4">
                             <span className="">Title:</span>
-                            <div className="flex justify-center items-center mt-4">
+                            <div className="flex justify-center items-center mt-2">
                                 <input
                                     onChange={evt => this.props.onEdit && this.props.onEdit(evt, 'name')}
                                     value={this.props.node?.name || ''}
                                     type="text"
-                                    className="rounded w-full"
+                                    className="rounded w-full p-2"
                                 ></input>
                             </div>
                         </div>
                         <div className="mt-4">
-                            <span className="">Description:</span>
-                            <div className="flex justify-center items-center mt-4">
+                            <span className="">Notes:</span>
+                            <div className="flex justify-center items-center mt-2">
                                 <textarea
                                     onChange={evt => this.props.onEdit && this.props.onEdit(evt, 'description')}
                                     value={this.props.node?.description || ''}
-                                    className="rounded w-full"
+                                    className="rounded w-full p-2"
                                 ></textarea>
                             </div>
                         </div>
                     </>
                 }
-                <div className="mt-4">
-                    <button
-                        onClick={this.props.onClose}
-                        className="rounded px-2 py-1 border border-blue-700"
-                    >
-                        Close
-                    </button>
-                </div>
             </div>
         );
     }
