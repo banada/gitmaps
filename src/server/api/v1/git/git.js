23c208e55e1023187f30a25b9de30462a408c3d1
Lawrence Chau <lawrence.j.chau@gmail.com>
2022-02-26 17:58:19 -0800

    Initial commit

--- src/server/api/v1/git/git.js
+++ src/server/api/v1/git/git.js
@@ -4,6 +4,13 @@ import gitController from '../../../controllers/git/gitController';
 const git = express.Router();
 
 /**
+ * Routes a repo creation request 
+ */
+git.post('/repo', async (req, res, next) => {
+    return await gitController.createRepo(req, res, next);
+});
+
+/**
  *
  */
 git.get('/', async (req, res, next) => {
