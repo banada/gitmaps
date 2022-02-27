098b61aa656f77019adc0d3430d1dd30f529446f
Nathaniel Chen <nathanielchen@hey.com>
2022-02-26 21:38:00 -0800

    Get PR data with GitHub API
    Moved logic from client to server
    Call the GitHub API to get JSON blobs

--- src/server/controllers/files/filesController.js
+++ src/server/controllers/files/filesController.js
@@ -1,27 +1,93 @@
 import axios from 'axios';
+import Base64 from 'js-base64';
+import { Octokit } from 'octokit';
+import { createOAuthAppAuth } from '@octokit/auth-oauth-app';
 import { isUn } from '../../../utils/utils';
 
-const readFile = async (req, res, next) => {
-    const url = req.params[0];
-    if (isUn(url)) {
-        res.status(400);
-        return res.send();
+const GITMAP_EXT = '.json';
+
+const octokit = new Octokit({
+    authStrategy: createOAuthAppAuth,
+    auth: {
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
     }
+});
+
+const readFile = async (req, res, next) => {
+
+    //TODO validate
+
+    const owner = req.params.user;
+    const repo = req.params.repo;
+    const pull_number = req.params.pr;
 
     try {
-        const result = await axios.get(url, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
+        // Get a pull request
+        const pullRequest = await octokit.rest.pulls.get({
+            owner,
+            repo,
+            pull_number
         });
-        if (isUn(result)) {
-            res.status(404);
-            return res.send();
+
+        const base = pullRequest.data.base;
+        const head = pullRequest.data.head;
+
+        // Get pull request file path
+        const prFiles = await octokit.rest.pulls.listFiles({
+            owner,
+            repo,
+            pull_number
+        });
+
+        //console.log(prFiles.data);
+        let matchedFilePath;
+        for (let i=0; i<prFiles.data.length; i++) {
+            const file = prFiles.data[i];
+            const parts = file.filename.split('/');
+            if (parts[parts.length-1].includes(GITMAP_EXT)) {
+                matchedFilePath = file.filename;
+            }
+            break;
         }
 
-        res.status(200);
-        return res.json(result.data);
+        // Get files from each branch
+        const baseFile = await octokit.rest.repos.getContent({
+            owner: base.user.login,
+            repo,
+            path: matchedFilePath,
+            ref: base.ref
+        });
+        const headFile = await octokit.rest.repos.getContent({
+            owner: head.user.login,
+            repo,
+            path: matchedFilePath,
+            ref: head.ref
+        });
 
+        // Get gitmap blob
+        const baseBlob = await octokit.rest.git.getBlob({
+            owner: base.user.login,
+            repo,
+            file_sha: baseFile.data.sha
+        });
+        const headBlob = await octokit.rest.git.getBlob({
+            owner: head.user.login,
+            repo,
+            file_sha: headFile.data.sha
+        });
+
+        res.status(200);
+        return res.json({
+            base: {
+                data: Base64.decode(baseBlob.data.content),
+                ref: base.label
+            },
+            head: {
+                data: Base64.decode(headBlob.data.content),
+                ref: head.label
+            }
+        });
     } catch (err) {
         console.log(err);
         res.status(500);
