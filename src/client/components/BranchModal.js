348f4a6feb2c1e3ca84e7ab9500473367a351b30
Nathaniel Chen <nathanielchen@hey.com>
2022-03-03 21:29:23 -0800

    Modal styling

--- src/client/components/BranchModal.js
+++ src/client/components/BranchModal.js
@@ -19,6 +19,15 @@ class BranchModal extends React.Component {
         }
     }
 
+    onSubmit = (evt) => {
+        if (this.state.branch === 'New Branch') {
+            // TODO Create a new branch, then continue
+            // TODO Set state to new branch
+        }
+
+        this.props.onSelect(this.state.branch);
+    }
+
     render() {
         const branches = this.props.branches.map((b, idx) => {
             return (
@@ -37,23 +46,25 @@ class BranchModal extends React.Component {
                 <div
                     className="bg-white w-1/3 rounded z-50 mx-auto flex flex-col justify-center"
                 >
-                    <p className="text-2xl my-4">Choose a branch:</p>
-                    <div className="">
-                        <select onChange={this.onChangeBranch}>
+                    <p className="text-2xl mt-4">Choose a branch to save to:</p>
+                    <div className="my-4">
+                        <select
+                            className="p-2 rounded cursor-pointer"
+                            onChange={this.onChangeBranch}
+                        >
                             <option></option>
                             {branches}
+                            <option>New Branch</option>
                         </select>
                     </div>
-                    {(this.state.allowSubmit) &&
-                        <div className="flex justify-center">
-                            <div
-                                className="mt-4 p-2 rounded cursor-pointer bg-blue-200 w-1/2"
-                                onClick={e => this.props.onSelect(this.state.branch)}
-                            >
-                                Select
-                            </div>
+                    <div className="flex justify-center mb-4">
+                        <div
+                            className="mt-4 p-2 rounded cursor-pointer bg-blue-200 w-1/4"
+                            onClick={this.onSubmit}
+                        >
+                            Select
                         </div>
-                    }
+                    </div>
                 </div>
             </div>
         );
