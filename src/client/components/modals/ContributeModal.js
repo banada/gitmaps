1f3ac452bf872a0ed54aed5133185ec53c85019d
Nathaniel Chen <nathanielchen@hey.com>
2022-03-05 14:45:38 -0800

    Display BTC QR for contributions
    If a user puts btc.txt in the same directory as
    gitmap.json, we show a button that opens a contribution
    modal.

---
+++ src/client/components/modals/ContributeModal.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+import qr from 'qr-image';
+import CloseIcon from '../icons/CloseIcon';
+
+class ContributeModal extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onSubmit = async (evt) => {
+        const repo = this.newRepo.current.value;
+        const visibility = this.visibility.current.value;
+        if (repo === '') {
+            toast.error('Please choose a name for your repository.');
+        } else {
+            this.props.onSelect({repo, visibility});
+        }
+    }
+
+    render() {
+        const btcStr = `bitcoin:${this.props.address}?amount=${this.props.amount}`;
+        const qrImg = qr.imageSync(btcStr);
+        const qrData =  `data:image/png;base64,${qrImg.toString('base64')}`;
+
+        return (
+            <div className="w-full h-full fixed top-0 left-0 flex items-center text-center">
+                <div
+                    className="absolute w-full h-full bg-gray-900 opacity-50 cursor-pointer"
+                    onClick={this.props.onClose}
+                ></div>
+                <div
+                    className="bg-white w-1/2 rounded z-50 mx-auto flex flex-col justify-center"
+                >
+                    {/* Close Icon */}
+                    <div
+                        className="flex justify-end items-center cursor-pointer m-2"
+                        onClick={this.props.onClose}
+                    >
+                        <CloseIcon
+                            size={6}
+                        />
+                    </div>
+                    <p className="text-3xl mt-4">Contribute BTC</p>
+                    <div className="mt-4 flex justify-center">
+                        <img src={qrData} />
+                    </div>
+                    <p className="text-lg mt-4 mb-8 px-4">Scan to fund this project with Bitcoin. The project maintainer uses these funds to reward people who contribute to this project through pull requests.</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ContributeModal;
+
